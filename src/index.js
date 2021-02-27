0ce93841068d5b154c683262c08aecf2104e8a59
DESKTOP-GGKHKBC\PEDRO <diegoedgardosalazarvergara@gmail.com>
2021-02-27 11:12:11 -0500

    añadí más info

--- src/index.js
+++ src/index.js
@@ -1,145 +1,125 @@
-window.onload = function(){
-    $('#onload').fadeOut();
+window.onload = function () {
+  $("#onload").fadeOut();
 
-
-    let start = document.getElementById('start');
-    start.onclick = () =>{
-        document.getElementById('startContainer').style.animationDuration = '.7s';
-        document.getElementById('startContainer').style.animationName = 'hide';
-        setTimeout(() => {
-            document.getElementById('startContainer').style.display = 'none';
-            document.getElementById('start').style.animationName = 'show';
-            document.getElementById('main').style.display='block';
-        }, 700);
-        $('body').removeClass('hidden');
-    }
-
-    
+  let start = document.getElementById("start");
+  start.onclick = () => {
+    document.getElementById("startContainer").style.animationDuration = ".7s";
+    document.getElementById("startContainer").style.animationName = "hide";
+    setTimeout(() => {
+      document.getElementById("startContainer").style.display = "none";
+      document.getElementById("start").style.animationName = "show";
+      document.getElementById("main").style.display = "block";
+    }, 700);
+    $("body").removeClass("hidden");
+  };
+};
+function WhoIAM() {
+  document.getElementById("main").style.animationName = "hide";
+  setTimeout(() => {
+    let section = document.getElementById("section1");
+    section.style.display = "block";
+    document.getElementById("main").style.display = "none";
+    let back = document.getElementById("home1");
+    back.onclick = () => {
+      document.getElementById("main").style.animationName = "show";
+      section.style.animationName = "hide";
+      setTimeout(() => {
+        section.style.display = "none";
+        section.style.animationName = "show";
+        document.getElementById("main").style.display = "block";
+      }, 500);
+    };
+  }, 500);
 }
-function WhoIAM(){
-    document.getElementById('main').style.animationName='hide';
-    setTimeout( ()=>{
-        let section = document.getElementById('section1');
-        section.style.display='block';
-        document.getElementById("main").style.display='none';
-        let back = document.getElementById('home1');
-        back.onclick = () =>{
-            document.getElementById('main').style.animationName='show';
-            section.style.animationName = 'hide';
-            setTimeout( ()=>{
-                section.style.display='none';
-                section.style.animationName='show';
-                document.getElementById("main").style.display='block';
-            } , 500 );
-        }
-    },500);
-
-
+function WhatIDo() {
+  document.getElementById("main").style.animationName = "hide";
+  setTimeout(() => {
+    let section = document.getElementById("section2");
+    section.style.display = "block";
+    document.getElementById("main").style.display = "none";
+    let back = document.getElementById("home2");
+    back.onclick = () => {
+      document.getElementById("main").style.animationName = "show";
+      section.style.animationName = "hide";
+      setTimeout(() => {
+        section.style.display = "none";
+        section.style.animationName = "show";
+        document.getElementById("main").style.display = "block";
+      }, 500);
+    };
+  }, 500);
 }
-function WhatIDo(){
-
-    document.getElementById('main').style.animationName='hide';
-    setTimeout( ()=>{
-        let section = document.getElementById('section2');
-        section.style.display='block';
-        document.getElementById("main").style.display='none';
-        let back = document.getElementById('home2');
-        back.onclick = () =>{
-            document.getElementById('main').style.animationName='show';
-            section.style.animationName = 'hide';
-            setTimeout( ()=>{
-                section.style.display='none';
-                section.style.animationName='show';
-                document.getElementById("main").style.display='block';
-            } , 500 );
-        }
-    },500);
-
-
-
+function Blog() {
+  document.getElementById("main").style.animationName = "hide";
+  setTimeout(() => {
+    let section = document.getElementById("section3");
+    section.style.display = "block";
+    document.getElementById("main").style.display = "none";
+    let back = document.getElementById("home3");
+    back.onclick = () => {
+      document.getElementById("main").style.animationName = "show";
+      section.style.animationName = "hide";
+      setTimeout(() => {
+        section.style.display = "none";
+        section.style.animationName = "show";
+        document.getElementById("main").style.display = "block";
+      }, 500);
+    };
+  }, 500);
 }
-function Blog(){
-    document.getElementById('main').style.animationName='hide';
-    setTimeout( ()=>{
-        let section = document.getElementById('section3');
-        section.style.display='block';
-        document.getElementById("main").style.display='none';
-        let back = document.getElementById('home3');
-        back.onclick = () =>{
-            document.getElementById('main').style.animationName='show';
-            section.style.animationName = 'hide';
-            setTimeout( ()=>{
-                section.style.display='none';
-                section.style.animationName='show';
-                document.getElementById("main").style.display='block';
-            } , 500 );
-        }
-    },500);
-}
-function ContactMe(){
-    document.getElementById('main').style.animationName='hide';
-    setTimeout( ()=>{
-        let section = document.getElementById('section4');
-        section.style.display='block';
-        document.getElementById("main").style.display='none';
-        let back = document.getElementById('home4');
-        back.onclick = () =>{
-            document.getElementById('main').style.animationName='show';
-            section.style.animationName = 'hide';
-            setTimeout( ()=>{
-                section.style.display='none';
-                section.style.animationName='show';
-                document.getElementById("main").style.display='block';
-            } , 500 );
-        }
-    },500);
+function ContactMe() {
+  document.getElementById("main").style.animationName = "hide";
+  setTimeout(() => {
+    let section = document.getElementById("section4");
+    section.style.display = "block";
+    document.getElementById("main").style.display = "none";
+    let back = document.getElementById("home4");
+    back.onclick = () => {
+      document.getElementById("main").style.animationName = "show";
+      section.style.animationName = "hide";
+      setTimeout(() => {
+        section.style.display = "none";
+        section.style.animationName = "show";
+        document.getElementById("main").style.display = "block";
+      }, 500);
+    };
+  }, 500);
 }
 
-function Send(){
-
-    const data = {
-        sID  : "service_jw0i0sw",
-        tID  : "template_v4k67yq",
-        name : "Decklan"
-
-    }
-    let name = document.getElementById('name').value;
-    let affair = document.getElementById('affair').value;
-    let email = document.getElementById('email').value;
-    let text = document.getElementById('message').value;
-    emailjs.send(data.sID ,data.tID,{
-        from_name: name,
-        to_name: data.name,
-        from_email: email,
-        from_affair: affair,
-        message: text
-    });
-    swal("Sent", "Decklan got your message", "success");
-    document.getElementById('name').value = '';
-    document.getElementById('affair').value = '';
-    document.getElementById('email').value = '';
-    document.getElementById('message').value = '';
-    
+function Send() {
+  const data = {
+    sID: "service_jw0i0sw",
+    tID: "template_v4k67yq",
+    name: "Decklan",
+  };
+  let name = document.getElementById("name").value;
+  let affair = document.getElementById("affair").value;
+  let email = document.getElementById("email").value;
+  let text = document.getElementById("message").value;
+  emailjs.send(data.sID, data.tID, {
+    from_name: name,
+    to_name: data.name,
+    from_email: email,
+    from_affair: affair,
+    message: text,
+  });
+  swal("Sent", "Decklan got your message", "success");
+  document.getElementById("name").value = "";
+  document.getElementById("affair").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("message").value = "";
 }
 
-
-function Start(){
-    let start = document.getElementById('start');
-    start.onclick = () =>{
-        document.getElementById('startContainer').style.animationDuration = '.7s';
-        document.getElementById('startContainer').style.animationName = 'hide';
-        setTimeout(() => {
-            document.getElementById('startContainer').style.display = 'none';
-            document.getElementById('start').style.animationName = 'show';
-            document.getElementById('main').style.display='block';
-        }, 700);
-    }
-    $('body').removeClass('hidden');
+function Start() {
+  let start = document.getElementById("start");
+  start.onclick = () => {
+    document.getElementById("startContainer").style.animationDuration = ".7s";
+    document.getElementById("startContainer").style.animationName = "hide";
+    setTimeout(() => {
+      document.getElementById("startContainer").style.display = "none";
+      document.getElementById("start").style.animationName = "show";
+      document.getElementById("main").style.display = "block";
+    }, 700);
+  };
+  $("body").removeClass("hidden");
 }
-
-
-
-
-
-
-
